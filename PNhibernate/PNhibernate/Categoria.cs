2f51eac3cb021cd38ad986dc1d22e86280d29ef9
KevinSotomayor <pelmuss@gmail.com>
2013-02-01 10:32:36 +0100

    Subiendo PNhibernate

---
+++ PNhibernate/PNhibernate/Categoria.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Serpis.Ad
+{
+	public class Categoria
+	{
+		public long Id {get; set;}
+		public string Nombre{get; set;}
+			
+//		que es aprox:
+//		private string nombre;
+//		public string Nombre{
+//			get {return nombre;}
+//			set {nombre = value;}
+//		}
+	}
+}
+
--- PNhibernate/PNhibernate/MainWindow.cs
+++ PNhibernate/PNhibernate/MainWindow.cs
@@ -1,18 +1,41 @@
 using System;
 using Gtk;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using Serpis.Ad;
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		
+		Configuration configuration = new Configuration();
+		configuration.Configure();
+		
+		configuration.AddAssembly(typeof(Categoria).Assembly);
+		
+		new SchemaExport(configuration).Execute(true, false, false);
+		
+		ISessionFactoryConfiguration sessionFactory = configuration.BuildSessionFactory();
+		
+		ISession session = sessionFactory.OpenSession;
+		
+		
+		Categoria categoria = (Categoria)session.Load(typeOf(Categoria),2L);
+		//2L constante = Long
+	
+		
+		session.Close();
+		sessionFactory.Close();
 	}
+	
 	
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
 		Application.Quit ();
 		a.RetVal = true;
-	//http://nhforge.org/wikis/howtonh/your-first-nhibernate-based-application.aspx
-	//http://nhforge.org/doc/nh/en/index.html
+		
 	}
 }
